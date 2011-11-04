31a73a99f22d3f8c5e214a9d8f5bdcd8f71a99b2
Olivier Dony <odo@openerp.com>
2011-11-04 15:44:17 +0100

    [IMP] release: update version to match recent change of server-side format

--- bin/release.py
+++ bin/release.py
@@ -21,7 +21,7 @@
 #
 
 name = 'openerp-client'
-version = '6.1dev'
+version = '6.1beta'
 major_version = '6.1'
 description = 'OpenERP Client'
 long_desc = '''OpenERP is a complete ERP and CRM. The main features are accounting (analytic
